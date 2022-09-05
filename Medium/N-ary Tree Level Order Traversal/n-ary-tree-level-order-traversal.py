06b4a142c72921713f08a97567b8cd91c2bb800a
George Plotnikov <accembler@gmail.com>
2022-09-05 08:12:40 +0500

    Create n-ary-tree-level-order-traversal.py

---
+++ Medium/N-ary Tree Level Order Traversal/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,23 @@
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if root is None:
+            return []
+        level = deque([root])
+        res = []
+        
+        while len(level) > 0:
+            
+            tmp_level = deque()
+            tmp_res = []
+            
+            while len(level) > 0:
+                node = level.popleft()
+                tmp_res.append(node.val)
+                for n in node.children:                    
+                    tmp_level.append(n)
+                
+            level = tmp_level
+            res.append(tmp_res)
+            
+        return res
+
