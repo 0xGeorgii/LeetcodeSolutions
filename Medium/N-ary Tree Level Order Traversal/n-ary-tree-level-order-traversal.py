88871571465d484a77a2370dde6f4b24d21d01c3
George Plotnikov <accembler@gmail.com>
2022-09-05 08:13:45 +0500

    Update n-ary-tree-level-order-traversal.py

--- Medium/N-ary Tree Level Order Traversal/n-ary-tree-level-order-traversal.py
+++ Medium/N-ary Tree Level Order Traversal/n-ary-tree-level-order-traversal.py
@@ -1,3 +1,10 @@
+"""
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
 class Solution:
     def levelOrder(self, root: 'Node') -> List[List[int]]:
         if root is None:
