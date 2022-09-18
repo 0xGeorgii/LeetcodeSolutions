ff317b78e3a421db42954a5a90538633bee0297b
George Plotnikov <accembler@gmail.com>
2022-09-18 20:12:51 +0500

    Create trapping-rain-water.py

---
+++ Hard/Trapping Rain Water/trapping-rain-water.py
@@ -0,0 +1,40 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        volume = 0
+        shift = 0
+        
+        landscape = []
+
+        def checkSegment(l, r, arr):
+            v = 0
+            edge = min(r, l)
+            while len(arr) > 0:
+                h = arr.pop()
+                if h > edge:
+                    edge = h
+                else:
+                    v += edge - h
+            return v
+        
+        for i in range(len(height)):
+            
+            if shift >= len(height):
+                break
+
+            left = height[shift]
+            right = 0
+            for j in range(1 + shift, len(height)):
+                right = height[j]
+                shift += 1
+                if right >= left:
+                    volume += checkSegment(left, right, landscape)
+                    shift = j
+                    break
+                else:
+                    landscape.append(right)
+            
+            if len(landscape) > 0:
+                volume += checkSegment(left, right, landscape)
+
+        return volume
