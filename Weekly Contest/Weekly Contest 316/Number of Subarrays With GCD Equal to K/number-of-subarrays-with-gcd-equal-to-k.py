e24fd24bdb5fee16311af4d0351a4e7d4914ce80
George Plotnikov <accembler@gmail.com>
2022-10-23 09:00:53 +0500

    Create number-of-subarrays-with-gcd-equal-to-k.py

---
+++ Weekly Contest/Weekly Contest 316/Number of Subarrays With GCD Equal to K/number-of-subarrays-with-gcd-equal-to-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarrayGCD(self, nums: List[int], k: int) -> int:
+                
+        res = 0
+        start = -1
+        
+        for i in range(len(nums)):
+            if start < 0 and nums[i] % k == 0:
+                start = i
+                if math.gcd(nums[i]) == k:
+                    res += 1
+            elif nums[i] % k == 0:
+                for j in range(start, i):
+                    if math.gcd(*nums[j:i+1]) == k:
+                        res += 1
+                if math.gcd(nums[i]) == k:
+                    res += 1
+            else:
+                start = -1
+        
+        return res
