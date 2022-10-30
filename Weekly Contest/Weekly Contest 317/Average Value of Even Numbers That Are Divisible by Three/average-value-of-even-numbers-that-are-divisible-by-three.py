ef7eca2487072846d89f0452d01ed6ebdd4d03d2
George Plotnikov <accembler@gmail.com>
2022-10-30 09:34:16 +0500

    Create average-value-of-even-numbers-that-are-divisible-by-three.py

---
+++ Weekly Contest/Weekly Contest 317/Average Value of Even Numbers That Are Divisible by Three/average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -0,0 +1,14 @@
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        
+        res = []
+        
+        for n in nums:
+            
+            if n % 2 == 0 and n % 3 == 0:
+                res.append(n)
+                
+        if len(res) == 0:
+            return 0
+        
+        return int(mean(res))
